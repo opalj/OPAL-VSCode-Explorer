11005bc18c807b1dd5561706a18881439273b0ec
Alexander Gössl <alexandergoessl@web.de>
2019-02-17 14:29:34 +0100

    moved tac service in service folder

--- extension/src/test/extension.test.ts
+++ extension/src/test/extension.test.ts
@@ -4,9 +4,9 @@
 //
 
 // The module 'assert' provides assertion methods from node
-var assert = require('chai').assert
-var expect = require('chai').expect
-import { TacService } from '../extension/tac.service';
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+import { TacService } from '../extension/service/tac.service';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
