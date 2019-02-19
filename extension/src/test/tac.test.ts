5d6ed25fd2f7d30cd8b48ee0c1d8ad3480bb6c82
Alexander Gössl <alexandergoessl@web.de>
2019-02-19 12:50:52 +0100

    tests will fail if server not available

--- extension/src/test/tac.test.ts
+++ extension/src/test/tac.test.ts
@@ -10,7 +10,7 @@ suite("TAC Test Suit", function () {
         var tacService = new TacService("http://localhost:8080/tac/");
         var id = 'short1.txt';
 
-        try {
+
             var res : any = await tacService.loadTAC(id);    
             expect(res).to.have.property('tac');
             expect(res).to.have.property('id');
@@ -18,16 +18,14 @@ suite("TAC Test Suit", function () {
             expect(res.tac).to.have.string('void <init>(){');
             expect(res.tac).to.have.string('param0: useSites={0} (origin=-1)');
             expect(res.tac).to.have.string('// ⚡️ <uncaught exception ⇒ abnormal return>');
-        } catch(error) {
-            console.log(error);
-        }
+
       });
 
 	test('assertion success severalMethods.txt', async () => {
         var tacService = new TacService("http://localhost:8080/tac/");
         var id = 'severalMethods.txt';
 
-        try {
+
             var res : any = await tacService.loadTAC(id);    
             expect(res).to.have.property('tac');
             expect(res).to.have.property('id');
@@ -35,16 +33,14 @@ suite("TAC Test Suit", function () {
             expect(res.tac).to.have.string('void a(java.lang.String){');
             expect(res.tac).to.have.string('7:/*pc=24:*/ lv7 = {param0}/*com.android.volley.VolleyLog$a*/.b');
             expect(res.tac).to.have.string('30:/*pc=96:*/ lv1e = "(%-4d ms) %s"');
-        } catch(error) {
-            console.log(error);
-        }
+
       });
 
 	test('assertion success intermediate2.txt', async () => {
         var tacService = new TacService("http://localhost:8080/tac/");
         var id = 'intermediate2.txt';
 
-        try {
+
             var res : any = await tacService.loadTAC(id);    
             expect(res).to.have.property('tac');
             expect(res).to.have.property('id');
@@ -52,8 +48,6 @@ suite("TAC Test Suit", function () {
             expect(res.tac).to.have.string('4:/*pc=14:*/ lv4 = "volley/0"');
             expect(res.tac).to.have.string('1:/*pc=5:*/');
             expect(res.tac).to.have.string('static com.android.volley');
-        } catch(error) {
-            console.log(error);
-        }
+
       });
 });
