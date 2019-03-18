746f63a02081bd0df275880d49c85776b7299682
Alexander Hering <al.hering97@gmail.com>
2019-03-18 15:34:40 +0100

    added opal config class

---
+++ extension/src/extension/opal.config.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+
+export default class OpalConfig {
+    /**
+     * Open opal.config.json
+    */
+    async getConfig() {
+        var rootPath = vscode.workspace.rootPath;
+	    var path: vscode.Uri = vscode.Uri.parse("file:"+rootPath+"/opal.config.json");
+	    var document = await vscode.workspace.openTextDocument(path);
+	    return JSON.parse(document.getText());
+    }
+    
+
+
+
+
+}
