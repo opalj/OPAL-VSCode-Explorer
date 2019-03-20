4caeecbcf1de8801bcaf93db7b0b77d3f5ba4f31
Alexander Hering <al.hering97@gmail.com>
2019-03-20 15:00:44 +0100

    implemented DocumentLinkProvider for tac provider

--- extension/src/extension/provider/tac.provider.ts
+++ extension/src/extension/provider/tac.provider.ts
@@ -1,8 +1,11 @@
 import * as vscode from 'vscode';
 import TACDocument from './tac.document';
+import {CommandService} from "../service/command.service";
+import OpalConfig from "../opal.config";
+import * as npmPath from 'path';
 
 
-export default class TACProvider implements vscode.TextDocumentContentProvider {
+export default class TACProvider implements vscode.TextDocumentContentProvider, vscode.DocumentLinkProvider {
 
     static scheme = 'tac';
 
@@ -28,6 +31,31 @@ export default class TACProvider implements vscode.TextDocumentContentProvider {
         
         this._documents.set(uri.toString(), document);
         return document.value;
+    }
+
+    /**
+    * Method for requesting links for a tac document
+     * 
+     * @param document document for which links are requested
+     * @param token cancellation token
+     */
+    async provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]>{
+        let config = await OpalConfig.getConfig(); 
+        let cmdService = new CommandService(config.server.url);
+        
+        var fileName = npmPath.parse(document.uri.fsPath).base;
+        var cmd = "getLinks";
+        var fqn = await cmdService.getFQN(document.uri.fsPath);
+            var getParams = {
+                "fqn": fqn,
+                "className":fileName
+            };
+
+        var res = await cmdService.loadAnyCommand(cmd, this.projectId, getParams);
+
+        var dummyDocLink = new vscode.DocumentLink(new vscode.Range(new vscode.Position(1,1), new vscode.Position(2,2)), document.uri);
+        var dummyArray = [dummyDocLink];
+        return dummyArray;
     }
 }
 
