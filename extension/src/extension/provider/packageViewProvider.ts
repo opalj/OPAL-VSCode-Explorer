4055ba93a428b4e6377218bcc4b82de8568447f3
Alexander Hering <al.hering97@gmail.com>
2019-03-25 13:17:53 +0100

    finished TreeProvider

--- extension/src/extension/provider/packageViewProvider.ts
+++ extension/src/extension/provider/packageViewProvider.ts
@@ -20,31 +20,38 @@ export class PackageViewProvider implements vscode.TreeDataProvider<Package> {
 											vscode.TreeItemCollapsibleState.Collapsed, 
 											projectFolder.fsPath);
 		}
-		
-		
 	}
 
 	public refresh(): void {
 		this._onDidChangeTreeData.fire();
 	}
 
-	public getTreeItem(p: Package): vscode.TreeItem {
+	public getTreeItem(p: Package): vscode.TreeItem | Thenable<vscode.TreeItem>{
 		return p;
 	}
 
-	public getChildren(p?: Package): Thenable<Package[]> {
+	public getChildren(p?: Package): vscode.ProviderResult<Package[]> {
+		console.log(p);
 		if(p){
 			if (p.hasSubPackages()) {
 				return Promise.resolve(p.getChildren());
 			} else {
 				return Promise.resolve([]);
+				vscode.window.showInformationMessage('Kein Subpackage enthalten!');
 			}
 		} else {
-			vscode.window.showInformationMessage('Kein Subpackage enthalten!');
-			return Promise.resolve([]);
+			let p = this.setPackageTree(this._projectFolder.fsPath);
+			if(p){
+				return Promise.resolve(p.getChildren());
+			} else {
+				return Promise.resolve([]);
+			}
+			
 		}
-		
+	}
 
+	public getParent(p: Package): vscode.ProviderResult<Package>{
+		return new Promise(p.getParent);
 	}
 
 	public setPackageTree(root : string)  : Package | undefined {
@@ -52,21 +59,22 @@ export class PackageViewProvider implements vscode.TreeDataProvider<Package> {
 		const tree = dirTree(root, {normalizePath:true});
 		var subPackages: Package[];
 		subPackages = [];
-
 		if(tree.type === "directory"){
 			for(let i = 0; i < tree.children.length; i++){
+				
 				if(tree.children[i].type === "directory"){
 					let resTmp = this.setPackageTree(tree.children[i].path);
 					if(resTmp){
 						subPackages.push(resTmp);
 					}
-					
 				}
 			}
 			let res : Package;
 				res = new Package(tree.name, vscode.TreeItemCollapsibleState.Collapsed, tree.path);
 				res.setChildren(subPackages);
-				vscode.window.showInformationMessage('Verarbeitet: ' + res.getName());
+				for(let i = 0; i < res.getChildren().length; i++){
+					res.getChildren()[i].setParent(res);
+				}
 				return res;
 		} 		
 	}
