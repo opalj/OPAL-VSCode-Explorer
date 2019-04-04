fcac00d0596fda1a39ee88b0be9154735bccd534
Alexander Gössl <alexandergoessl@web.de>
2019-04-04 22:24:22 +0200

    new class params converter

---
+++ extension/src/extension/service/params.converter.service.ts
@@ -0,0 +1,44 @@
+export class ParamsConverterService {
+    /**
+     * Get fqn path for file from its path
+     * @param targetFilePath Path to target file
+     * @param targetsDir Directory for the targets
+     */
+    static getFQN(targetFilePath : string, targetsDir : string) : string {
+        let targetFileParts = this.getPathParts(targetFilePath);
+        let targetsDirParts = this.getPathParts(targetsDir);
+
+        let path = "";
+        if (targetsDir[0] === "/") {
+            // number of directories to the target dir is equal to targetsDirParts.length
+            targetFileParts = targetFileParts.slice(targetsDirParts.length);
+        } else {
+            // c: is counted as directory
+            targetFileParts = targetFileParts.slice(targetsDirParts.length-1);
+        }
+
+        if (targetFileParts[0] === "classes") {
+            targetFileParts = targetFileParts.slice(1);
+        }
+
+        if (targetFileParts[targetFileParts.length-1] === "class") {
+            targetFileParts.splice(-1);
+        }
+
+        path = targetFileParts.join("/");
+        return path;
+    }
+
+    static getPathParts(path : string) {
+        const regex = /[a-zA-Z0-9]+/gm;
+        let match;
+        let result = [];
+        while ((match = regex.exec(path)) !== null) {
+            if (match.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            result.push(match[0]);
+        }
+        return result;
+    }
+}
