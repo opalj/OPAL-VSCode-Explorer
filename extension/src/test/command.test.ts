24313e4d96a82946449ef273bb411aa8876ecd3d
Alexander Gössl <alexandergoessl@web.de>
2019-04-05 22:52:38 +0200

    unit tests for get fqn method

--- extension/src/test/command.test.ts
+++ extension/src/test/command.test.ts
@@ -6,19 +6,19 @@ import { ParamsConverterService } from '../extension/service/params.converter.se
 suite("Command Test Suit", function() {
   test("get fqn from path windows",  () => {
     var filePath = "\\Users\\Alexander\\Documents\\asep\\vscode_plugin\\opal-vscode-explorer\\server\\target\\scala-2.12\\classes\\opal\\extension\\vscode\\OPALProject.class";
-    var targetsDir = "c:\\Users\\Alexander\\Documents\\asep\\vscode_plugin\\opal-vscode-explorer\\server\\target\\scala-2.12\\classes";
+    ParamsConverterService.targetsRoot = "c:\\Users\\Alexander\\Documents\\asep\\vscode_plugin\\opal-vscode-explorer\\server\\target\\scala-2.12\\classes";
 
     
-    var fqn = ParamsConverterService.getFQN(filePath, targetsDir);
+    var fqn = ParamsConverterService.getFQN(filePath);
     expect(fqn).to.equal("opal/extension/vscode/OPALProject");
   });
 
   test("get fqn from path linux",  () => {
     var filePath = "/home/alex/opal-vscode-explorer/dummy/Test.class";
-    var targetsDir = "/home/alex/opal-vscode-explorer/dummy/";
+    ParamsConverterService.targetsRoot = "/home/alex/opal-vscode-explorer/dummy/";
 
     
-    var fqn = ParamsConverterService.getFQN(filePath, targetsDir);
+    var fqn = ParamsConverterService.getFQN(filePath);
     expect(fqn).to.equal("Test");
   });
 });
